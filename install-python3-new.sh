d130101f6e366f97ef33775a693823cab476ddb4
Jess Primero <jprimero@packetbender.net>
2021-07-22 18:01:52 +0800

    new python3 install script

---
+++ install-python3-new.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+VER=3.9
+FULL_VER=$VER.0
+curl -O https://www.python.org/ftp/python/$FULL_VER/Python-$FULL_VER.tgz
+mkdir -p $HOME/usr/local
+export PATH=$HOME/usr/local/bin:$PATH
+tar -zxvf $HOME/Python-$FULL_VER.tgz
+cd $HOME/Python-$FULL_VER
+./configure
+make altinstall prefix=$HOME/usr/local exec-prefix=$HOME/usr/local
+cd $HOME/usr/local/bin/
+ln -s ./python$VER python3
+echo "export PATH=\$HOME/usr/local/bin:\$PATH" >> $HOME/.bashrc
