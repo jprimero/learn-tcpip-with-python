eadecf2312eb22e9459f8dbee26cc5278f68fd1b
Jess Primero <jprimero@packetbender.net>
2021-07-23 12:47:01 +0800

    modify setting the PATH variable

--- install-python3.sh
+++ install-python3.sh
@@ -3,11 +3,11 @@ VER=$1
 FULL_VER=$VER.0
 curl -O https://www.python.org/ftp/python/$FULL_VER/Python-$FULL_VER.tgz
 mkdir -p $HOME/usr/local
-export PATH=$HOME/usr/local/bin:$PATH
+echo "export PATH=\$HOME/usr/local/bin:\$PATH" >> $HOME/.bashrc
+source $HOME/.bashrc
 tar -zxvf $HOME/Python-$FULL_VER.tgz
 cd $HOME/Python-$FULL_VER
 ./configure
 make altinstall prefix=$HOME/usr/local exec-prefix=$HOME/usr/local
 cd $HOME/usr/local/bin/
 ln -s ./python$VER python3
-echo "export PATH=\$HOME/usr/local/bin:\$PATH" >> $HOME/.bashrc
